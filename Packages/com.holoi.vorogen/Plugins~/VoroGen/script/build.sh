ef7995188f3b53126598c714367898f09749f351
Botao Hu <me@botao.hu>
2023-11-18 11:05:31 +0300

    Add x86 64 for the plugins

--- Packages/com.holoi.vorogen/Plugins~/VoroGen/script/build.sh
+++ Packages/com.holoi.vorogen/Plugins~/VoroGen/script/build.sh
@@ -6,53 +6,48 @@ AR=ar
 
 INCLUDES="-I../ThirdParty/voro++-0.4.6/src"
 SOURCES="../ThirdParty/voro++-0.4.6/src"
-VORO_OBJS="cell.o common.o container.o unitcell.o v_compute.o c_loops.o v_base.o wall.o pre_container.o container_prd.o "
 
 rm -rf *.o *.so *.a *.bundle *.dylib 
 set -x
 
 ## MacOS 
+# arm64
 MAC_ROOT=`xcrun --sdk macosx --show-sdk-path`
-MAC_ARGS="--sysroot $MAC_ROOT -isysroot $MAC_ROOT -fPIC"
+ARCH_TARGET="-target arm64-apple-macos"
+MAC_ARGS="$ARCH_TARGET --sysroot $MAC_ROOT -isysroot $MAC_ROOT -fPIC"
 
 $CC $CFLAGS $INCLUDES $MAC_ARGS -c ../VoroGen/VoroGen.cc -o VoroGen.o 
-$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/cell.cc -o cell.o 
-$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/common.cc -o common.o 
-$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/container.cc -o container.o 
-$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/unitcell.cc -o unitcell.o 
-$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/v_compute.cc -o v_compute.o 
-$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/c_loops.cc -o c_loops.o 
-$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/v_base.cc -o v_base.o 
-$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/wall.cc -o wall.o 
-$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/pre_container.cc -o pre_container.o 
-$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/container_prd.cc -o container_prd.o 
+$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/voro++.cc -o voro++.o 
 
-$CC -fPIC -rdynamic -shared -o libVoroGen.dylib VoroGen.o $VORO_OBJS -lstdc++ 
-lipo -create -output VoroGen.bundle libVoroGen.dylib
+$CC $MAC_ARGS -fPIC -rdynamic -shared -o libVoroGen_arm64.dylib VoroGen.o voro++.o -lstdc++ 
+
+## x86_64
+MAC_ROOT=`xcrun --sdk macosx --show-sdk-path`
+ARCH_TARGET="-target x86_64-apple-macos"
+MAC_ARGS="$ARCH_TARGET --sysroot $MAC_ROOT -isysroot $MAC_ROOT -fPIC"
+
+$CC $CFLAGS $INCLUDES $MAC_ARGS -c ../VoroGen/VoroGen.cc -o VoroGen.o 
+$CC $CFLAGS $INCLUDES $MAC_ARGS -c ${SOURCES}/voro++.cc -o voro++.o 
+
+$CC $MAC_ARGS -fPIC -rdynamic -shared -o libVoroGen_x86_64.dylib VoroGen.o voro++.o -lstdc++ 
+
+lipo -create -output VoroGen.bundle libVoroGen_arm64.dylib libVoroGen_x86_64.dylib
 
 DST="../../../Runtime/macOS"
 rm -rf $DST/VoroGen.bundle
 cp -r VoroGen.bundle $DST
 
-# # iOS  
+# iOS  
 IOS_ROOT=`xcrun --sdk iphoneos --show-sdk-path`
-IOS_ARGS="-target arm64-apple-ios --sysroot $IOS_ROOT -isysroot $IOS_ROOT -fembed-bitcode"
+ARCH_TARGET="-target arm64-apple-ios"
+IOS_ARGS="--sysroot $IOS_ROOT -isysroot $IOS_ROOT -fembed-bitcode"
 
 $CC $CFLAGS $INCLUDES $IOS_ARGS -c ../VoroGen/VoroGen.cc -o VoroGen.o 
-$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/cell.cc -o cell.o 
-$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/common.cc -o common.o 
-$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/container.cc -o container.o 
-$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/unitcell.cc -o unitcell.o 
-$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/v_compute.cc -o v_compute.o 
-$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/c_loops.cc -o c_loops.o 
-$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/v_base.cc -o v_base.o 
-$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/wall.cc -o wall.o 
-$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/pre_container.cc -o pre_container.o 
-$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/container_prd.cc -o container_prd.o 
+$CC $CFLAGS $INCLUDES $IOS_ARGS -c ${SOURCES}/voro++.cc -o voro++.o 
 
-$AR -crv libVoroGen.a VoroGen.o $VORO_OBJS
+$AR -crv libVoroGen.a VoroGen.o voro++.o 
 
 DST="../../../Runtime/iOS"
 cp libVoroGen.a $DST
 
-rm -rf *.o *.so *.a *.bundle *.dylib 
+#rm -rf *.o *.so *.a *.bundle *.dylib 
